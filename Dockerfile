f5e4a7041742b77233a0931fd6b40fd859491c8c
keychi25 <e165412y@st.u-gakugei.ac.jp>
2020-03-24 13:28:52 +0900

    update 開発環境
    mysql -> 8.0
    java  -> 11

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
-FROM openjdk:jdk-alpine
+FROM adoptopenjdk/openjdk11:alpine
 
 ENV APP_PATH /usr/local/Sample
-ENV DB_PATH jdbc:mysql://sample_db:3306/sample_gradle?useUnicode=true&characterEncoding=UTF-8&useSSL=false
+ENV DB_PATH jdbc:mysql://sample_db:3399/sample_gradle?useUnicode=true&characterEncoding=UTF-8&useSSL=false
 
 WORKDIR ${APP_PATH}
 RUN mkdir -p ${APP_PATH}
-COPY . ${APP_PATHc}
+COPY . ${APP_PATH}
 
-RUN mkdir ./docs/match
-RUN touch ./docs/match/match.txt
-RUN ./gradlew wrapper
-RUN ./gradlew build
-# RUN ./gradlew build --scan
-RUN ./gradlew gLA
-ENTRYPOINT ./gradlew tRW
+ENTRYPOINT ./gradlew tR
--- src/main/java/utility/DriverAccessor.java
+++ src/main/java/utility/DriverAccessor.java
@@ -5,21 +5,22 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DriverAccessor {
-
-    //使用するDBの名前
-    //private final static String  DRIVER_URL = "jdbc:mysql://sample_db:3306/sample_gradle?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
-    private final static String DRIVER_NAME = "com.mysql.jdbc.Driver";
-    //自分のmysqlのユーザー名
+    // mysql5.7
+    //private final static String DRIVER_NAME = "com.mysql.jdbc.Driver";
+    // mysql8.0
+    private final static String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
+    // 自分のユーザ名
     private final static String USER_NAME = "root";
     //自分のmysqlのパスワード
-    private final static String PASSWORD = "root";
+    private final static String PASSWORD = "";
 
     public Connection createConnection() {
         String DRIVER_URL = System.getenv("DB_PATH");
         if(DRIVER_URL != null){
             System.out.println("リモート");
+            DRIVER_URL = "jdbc:mysql://localhost:3399/sample_gradle?characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9:00&rewriteBatchedStatements=true";
         }else{
-            DRIVER_URL = "jdbc:mysql://localhost:3399/sample_gradle?useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+            DRIVER_URL = "jdbc:mysql://localhost:3306/sample_gradle?characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9:00&rewriteBatchedStatements=true";
             System.out.println("ローカル");
         }
         try {
@@ -28,8 +29,11 @@ public class DriverAccessor {
             return con;
         } catch (ClassNotFoundException e) {
             System.out.println("Can't Find JDBC Driver.\n");
-        } catch (SQLException e) {
-            System.out.println("Connection Error.\n");
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
         }
         return null;
     }
